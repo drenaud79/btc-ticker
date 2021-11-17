92c4fd9ff0029400513583320f26b33d044d47ce
Denis Renaud <denis.renaud.ext@maaf.fr>
2021-11-17 13:35:29 +0100

    Create first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  
+
+  /*
+  fetch('flowers.jpg')
+  .then(function(response) {
+    return response.blob();
+  })
+  .then(function(myBlob) {
+    const objectURL = URL.createObjectURL(myBlob);
+    myImage.src = objectURL;
+  });*/
+
+  let url = "http://192.168.81.61:8080/Ticker"
+
+  const [bitcoin, setBitcoin] = useState(0)
+
+  useEffect(() => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const time = setTimeout(() => {
+          
+      setBitcoin(data.result.XXBTZEUR.a[0])
+      console.log(data.result.XXBTZEUR.a[0])
+      }, 2000);
+    });
+  
+  }, [])
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +41,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+       {bitcoin}
         <a
           className="App-link"
           href="https://reactjs.org"
